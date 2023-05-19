e2eecabda873bc58449f85bc09b135332a66ffee
timgall <119544130+timgall@users.noreply.github.com>
2023-05-19 14:59:28 -0500

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
-import pg from "pg";
+import pg, { Pool } from "pg";
 import dotenv from "dotenv";
+// import axios from "axios";
 
 dotenv.config();
 
@@ -13,6 +14,55 @@ const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+server.get("/api/userInfo", (req, res) => {
+  db.query("SELECT * FROM users")
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      res.status(500).send("Iternal Server Error");
+    });
+});
+server.get("/api/userInfo/:id", (req, res) => {
+  db.query("SELECT * FROM users WHERE id = $1", [id])
+    .then((result) => {
+      res.send(result.rows[0]);
+    })
+    .catch((err) => {
+      res.send(500).send("Internal Server Error");
+    });
+});
+
+server.patch("/api/userInfo/:id", (req, res) => {
+  const { id } = req.params;
+  const { author, experience, linkedIn, adminVerifier } = req.body;
+  Pool.query(
+    "UPDATE userInfo SET author=COALESCE($1, author), experience=COALESCE($2, experience), linkedIn=COALESCE($3,linkedIn), adminVerifier=COALESCE($4,adminVerifier) WHERE id=$5 RETURNING *",
+
+    [author, experience, linkedIn, adminVerifier, id]
+  )
+    .then((result) => {
+      res.send(result.rows[0]);
+    })
+    .catch(next);
+});
+server.delete("/api/userInfo/:id", (req, res) => {
+  const { id } = req.params;
+  Pool.query("SELECT * FROM userInfo WHERE id = $1", [id])
+    .then((result) => {
+      res.send(result.rows[0]);
+    })
+    .catch(next);
+});
+server.use((err, req, res, next) => {
+  console.error(err.message);
+  res.sendStatus(500);
+});
 server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+// async function getAllUserInfo(){
+//   const response= await axios.get("http://localhost:3050/userInfo");
+//   return response.data
+// }
+// getAllUserInfo();
